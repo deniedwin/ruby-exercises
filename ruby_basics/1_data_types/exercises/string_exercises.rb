dd08c29f036c749b0b5686364b54448221b9c07d
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-15 14:34:46 +0100

    Passed all the tests for the basic datatypes
    In Ruby language

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -4,37 +4,46 @@ def concatenate_example(string)
 end
 
 def concatenate(string)
+  "Hello " << string << "!"
   # use concatenation to format the result to be "Hello <string>!"
 end
 
 def substrings(word)
+  return word.slice!(0..3)
   # return the first 4 letters from the word using substrings
 end
 
 def capitalize(word)
+  return word.capitalize
   # capitalize the first letter of the word
 end
 
 def uppercase(string)
+  return string.upcase
   # uppercase all letters in the string
 end
 
 def downcase(string)
+  return string.downcase
   # downcase all letters in the string
 end
 
 def empty_string(string)
+  return string.empty?
   # return true if the string is empty
 end
 
 def string_length(string)
+  return string.length
   # return the length of the string
 end
 
 def reverse(string)
+  return string.reverse
   # return the same string, with all of its characters reversed
 end
 
 def space_remover(string)
+  return string.gsub(" ", "")
   # remove all the spaces in the string using gsub
 end
