4b9b508f24acab4b616804c5f99bd982673b6ccc
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-20 10:50:45 +0100

    Made function to check if items in the array
    Are in upcase or not

--- ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
+++ ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
@@ -4,14 +4,21 @@ def coffee_drink?(drink_list)
 end
 
 def correct_guess?(guess_list, answer)
+  guess_list.any?(answer)
   # use #any? to return true when any element of the guess_list (array) equals the answer (number)
 end
 
 def twenty_first_century_years?(year_list)
+  year_list.all? do |item|
+    (item >= 2001) && (item <= 2100)
+  end
   # use #all? to return true when all of the years in the year_list (array) are between 2001 and 2100
 end
 
 def correct_format?(word_list)
+  word_list.none? do |item|
+    item == item.upcase
+  end
   # use #none? to return true when none of the words in the word_list (array) are in upcase
 end
 
